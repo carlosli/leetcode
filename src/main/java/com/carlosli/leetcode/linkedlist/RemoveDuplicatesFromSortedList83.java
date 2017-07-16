320a48516ebd9952f9c5c805566e99346050509d
carlosli <carlosli@mail>
2017-07-16 15:32:05 +0800

    update Remove Duplicates From Sorted List 83

--- src/main/java/com/carlosli/leetcode/linkedlist/RemoveDuplicatesFromSortedList83.java
+++ src/main/java/com/carlosli/leetcode/linkedlist/RemoveDuplicatesFromSortedList83.java
@@ -5,9 +5,18 @@ package com.carlosli.leetcode.linkedlist;
  */
 public class RemoveDuplicatesFromSortedList83 {
     public static ListNode deleteDuplicates(ListNode head) {
+        /**
+         * 这里与
+         * @see RemoveDuplicatesFromSortedListII82
+         * 不同的地方在于dummy直接指向了head，而不是dummy.next指向了head
+         * 原因是相同的点并不会完全删除，总会保留一点，故而不会使dummy指向错误的list开头
+         * 但最好改为与82题相同的方式，避免错误
+         */
         ListNode dummy = head;
         while (head != null && head.next != null) {
+            // 判断后面两个是否相同
             if (head.next.val == head.val) {
+                // 删除第一个
                 head.next = head.next.next;
             } else {
                 head = head.next;
@@ -18,7 +27,7 @@ public class RemoveDuplicatesFromSortedList83 {
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
+        ListNode node2 = new ListNode(1);
         ListNode node3 = new ListNode(2);
         ListNode node4 = new ListNode(3);
         ListNode node5 = new ListNode(3);
